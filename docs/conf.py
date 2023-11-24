e855432f13599d4b265b98ac40f6eba4f6827e48
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-11-24 15:50:22 +0100

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "furo"
-html_static_path = ['_static']
-html_extra_path = ['_sources']
+html_theme = "furo"
