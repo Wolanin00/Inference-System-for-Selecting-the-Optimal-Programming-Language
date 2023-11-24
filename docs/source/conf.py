b7b1d1e6c91337140a2384b92128f49bbd270b07
Mateusz Szewczyk <szewczykmateusz64@gmail.com>
2023-11-24 13:28:00 +0000

    Auto-commit changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
